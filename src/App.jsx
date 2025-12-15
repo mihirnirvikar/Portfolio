e2f177cd6baa869ddf568547437163a6f2781cb6
Mihir Nirvikar <mihirnirvikar3@gmail.com>
2025-12-15 20:52:13 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import {Body} from './components/Body.jsx';
+
+export const App = () => {
+  return (
+    <div>
+      <Body />
+    </div>
+  );
+};
+
---
+++ src/components/Body.jsx
@@ -0,0 +1,7 @@
+export const Body = () => {
+    return (
+        <div>
+            <h1>I am a body</h1>
+        </div>
+    )
+}
