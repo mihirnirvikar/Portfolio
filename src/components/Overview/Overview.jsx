77330cc4df4f0f0161f853a046e7782935a1e008
Mihir Nirvikar <mihirnirvikar3@gmail.com>
2026-01-29 14:01:28 +0530

    refactor: minor changes

--- src/components/Overview/Overview.jsx
+++ src/components/Overview/Overview.jsx
@@ -10,7 +10,7 @@ export const Overview = () => {
   return (
     <>
       <div className="w-full lg:mr-24 md:mr-5 lg:ml-10 md:ml-5">
-        <div className="overview-container border mt-8 w-full py-3 px-4 rounded-lg mb-10">
+        <div className="overview-container border mt-8 w-full py-3 px-4 rounded-lg mb-10 ">
           <Welcome />
           <TechStack />
           <CurrentFocus />
